cb36d3c5ae21d0205e7381c23bb69326473dc765
KenKuan <kenkuan@hooloop.com>
2015-10-26 11:46:04 +0800

    Add framework target and share scheme

---
+++ AFDateHelperFramework/AFDateHelperFramework.h
@@ -0,0 +1,19 @@
+//
+//  AFDateHelperFramework.h
+//  AFDateHelperFramework
+//
+//  Created by Ken Kuan on 10/26/15.
+//  Copyright © 2015 All Forces. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFDateHelperFramework.
+FOUNDATION_EXPORT double AFDateHelperFrameworkVersionNumber;
+
+//! Project version string for AFDateHelperFramework.
+FOUNDATION_EXPORT const unsigned char AFDateHelperFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFDateHelperFramework/PublicHeader.h>
+
+
